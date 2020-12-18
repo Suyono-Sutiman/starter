dca63abe5706aeafaeb14f008ade17b15946c480
Suyono <yonoraphael@gmail.com>
2020-12-19 01:53:19 +0700

    tiga

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,8 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "./header"
+import Head2 from '../components/head2'
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -25,7 +18,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Head2 siteTitle={data.site.siteMetadata?.title || `Title`} style={{marginBottom:'2rem'}}/>
       <div>
         <main>{children}</main>
         <footer style={{
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,11 @@ import { Link } from "gatsby"
 import {Col, Image, Jumbotron, Row, Button} from 'react-bootstrap'
 import SEO from "../components/seo"
 import hero from '../images/startup.svg'
-import Head2 from "../components/head2"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from "../components/layout"
 
 const mgjb = {
   borderRadius:0,
-  marginBottom:0,
 }
 const bthr = {
   alignSelf: 'center',
@@ -16,8 +15,7 @@ const bthr = {
 }
 
 const IndexPage = () => (
-  <>
-  <Head2/>
+  <Layout>
     <SEO title="Home" />
     <Jumbotron className='bgsatu' style={mgjb}>
       <Row>
@@ -36,22 +34,7 @@ const IndexPage = () => (
         </Col>
       </Row>
     </Jumbotron>
-    <Jumbotron className='bgsatu' style={mgjb}>
-      <Row>
-        <Col className='htext' xs={12} md={6} >
-        <h3>Realine</h3>
-        <h1>Realisasi Mimpi Bisnis Online</h1>
-        <p>Membantu anda sukses di dunia bisnis online</p>
-        <Link to='/page-2'>
-        <Button variant='primary' size='lg' style={bthr}>Pilih Paket</Button>
-        </Link>
-        </Col>
-        <Col>
-        <Image src={hero} fluid style={{maxWidth:'60%'}} />
-        </Col>
-      </Row>
-    </Jumbotron>
-  </>
+  </Layout>
 )
 
 export default IndexPage
